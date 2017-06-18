c32cda4b949222319f1e44f789a674b9650bec04
José Ignacio Escribano <jiep@users.noreply.github.com>
2017-06-18 18:56:26 +0200

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -9,17 +9,17 @@ app.on('ready', () => {
   // Initialize the window to our specified dimensions
   win = new BrowserWindow({width: 1000, height: 600});
 
-  if (process.env.PACKAGE === 'true'){
+  //if (process.env.PACKAGE === 'true'){
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
       slashes: true
     }));
     win.webContents.openDevTools();
-  } else {
+  /*} else {
     win.loadURL(process.env.HOST);
     win.webContents.openDevTools();
-  }
+  }*/
 
   // Remove window once app is closed
   win.on('closed', () => {
